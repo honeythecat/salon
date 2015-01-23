f59005688a4ce7f786b2e4aba15bce433fa778fd
Heather <heather@thisisnotanorg.org>
2015-01-23 13:22:25 -0800

    Add app index and styling.

--- app.rb
+++ app.rb
@@ -8,5 +8,14 @@ require('pg')
 DB = PG.connect({:dbname => "salon"})
 
 get('/') do
+  @stylists = Stylist.all()
+  erb(:index)
+end
 
+post('/stylists') do
+  stylist_name = params.fetch("stylist_name")
+  stylist = Stylist.new({:stylist_name => stylist_name, :id => nil})
+  stylist.save()
+  @stylists = Stylist.all()
+  erb(:index)
 end
